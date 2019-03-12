61dc7735921adbb57173499adb7c2eb3b52ae741
Caio Rodrigues <rfcaio90@gmail.com>
2019-03-12 12:36:38 -0300

    jspf-aula-04

--- src/helpers/operators.js
+++ src/helpers/operators.js
@@ -1,6 +1,24 @@
 
-export const compose = (...fns) => value => fns.reduceRight((returnedValue, fn) => fn(returnedValue), value)
+export const compose = (...fns) => value => fns.reduceRight((returnedValue, fn) => {
+  return fn(returnedValue)
+}, value)
+
+export const debounceTime = (milliseconds, fn) => {
+  let timer = null
+  return () => {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(fn, milliseconds)
+  }
+}
 
 export const partialize = (fn, ...args) => fn.bind(null, ...args)
 
 export const pipe = (...fns) => value => fns.reduce((returnedValue, fn) => fn(returnedValue), value)
+
+export const takeUntil = (times, fn) => () => {
+  if (times-- > 0) {
+    fn()
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 
 import './helpers/array'
+import { debounceTime, partialize, pipe, takeUntil } from './helpers/operators'
 import { log } from './helpers/promise'
 import { NoteService } from './note/service'
 
-document.querySelector('button').addEventListener('click', () => {
-  NoteService.sumValuesByCode('2143')
-    .then(log)
-    .catch(log)
+let action = pipe(
+  partialize(takeUntil, 3),
+  partialize(debounceTime, 500)
+)
+
+let clickAction = action(() => {
+  NoteService.sumValuesByCode('2143').then(log).catch(log)
 })
+
+document.querySelector('button').addEventListener('click', clickAction)
